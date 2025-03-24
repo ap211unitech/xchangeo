59080ab1cb0359327731bbd7395a3086a763b488
Arjun Porwal <porwalarjun95@gmail.com>
2025-03-24 22:20:11 +0530

    feat: implement ERC20 faucet contract with token request and withdrawal functionality

---
+++ hardhat/contracts/faucet/Faucet.sol
@@ -0,0 +1,87 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.28;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+import "./IFaucet.sol";
+
+contract ERC20Faucet is IERC20Faucet, Ownable {
+    IERC20 private token;
+    uint256 private lockTime;
+    uint256 private withdrawalAmount;
+
+    mapping(address => uint256) private nextAccessTime;
+
+    constructor(
+        address _tokenAddress,
+        uint256 _lockTime,
+        uint256 _withdrawalAmount
+    ) Ownable(msg.sender) {
+        token = IERC20(_tokenAddress);
+        lockTime = _lockTime;
+        withdrawalAmount = _withdrawalAmount;
+    }
+
+    function requestTokens() external returns (bool) {
+        if (msg.sender == address(0)) {
+            revert Faucet__InvalidSender(
+                address(token),
+                msg.sender,
+                "Request must not originate from a zero account"
+            );
+        }
+
+        uint256 availableFunds = token.balanceOf(address(this));
+
+        if (availableFunds < withdrawalAmount) {
+            revert Faucet__InsufficientFunds(
+                address(token),
+                withdrawalAmount,
+                availableFunds,
+                "Insufficient funds in faucet for withdrawal request"
+            );
+        }
+
+        if (nextAccessTime[msg.sender] > block.timestamp) {
+            revert Faucet__InsufficientTimeElapsed(
+                address(token),
+                nextAccessTime[msg.sender],
+                lockTime,
+                block.timestamp,
+                "Insufficient time elapsed since last withdrawal - try again later."
+            );
+        }
+
+        nextAccessTime[msg.sender] = block.timestamp + lockTime;
+        token.transfer(msg.sender, withdrawalAmount);
+
+        emit Faucet__ReceivedFunds(address(this), msg.sender, withdrawalAmount);
+
+        return true;
+    }
+
+    function withdraw() external onlyOwner returns (bool) {
+        token.transfer(msg.sender, token.balanceOf(address(this)));
+        emit Faucet__Withdraw(
+            address(this),
+            msg.sender,
+            token.balanceOf(address(this))
+        );
+        return true;
+    }
+
+    function setWithdrawalAmount(uint256 _withdrawalAmount) external onlyOwner {
+        withdrawalAmount = _withdrawalAmount;
+    }
+
+    function setLockTime(uint256 _lockTime) external onlyOwner {
+        lockTime = _lockTime;
+    }
+
+    // Getters //
+    function getBalance() external view returns (uint256) {
+        return token.balanceOf(address(this));
+    }
+}
---
+++ hardhat/contracts/faucet/IFaucet.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.28;
+
+interface IERC20Faucet {
+    error Faucet__InvalidSender(
+        address tokenAddress,
+        address sender,
+        string description
+    );
+
+    error Faucet__InsufficientFunds(
+        address tokenAddress,
+        uint256 requestedAmount,
+        uint256 availableBalance,
+        string description
+    );
+
+    error Faucet__InsufficientTimeElapsed(
+        address tokenAddress,
+        uint256 lastRequestTime,
+        uint256 minWaitTime,
+        uint256 currentTime,
+        string description
+    );
+
+    // Emit when owner withdraw all tokens from faucet
+    event Faucet__ReceivedFunds(
+        address indexed from,
+        address indexed to,
+        uint256 indexed amount
+    );
+
+    // Emit when owner withdraw all tokens from faucet
+    event Faucet__Withdraw(
+        address indexed from,
+        address indexed to,
+        uint256 indexed amount
+    );
+
+    // Request tokens from faucet
+    function requestTokens() external returns (bool);
+
+    // Withdraw ERC20 tokens back to owner
+    function withdraw() external returns (bool);
+
+    // Set Withdrawal Amount for each request
+    function setWithdrawalAmount(uint256 withdrawalAmount) external;
+
+    // Set Lock time for each request
+    function setLockTime(uint256 lockTime) external;
+
+    // Get Balance of Faucet
+    function getBalance() external returns (uint256);
+}
