b598222c68a6bd529e35a2c90ac306aad7b20ee2
Arjun Porwal <porwalarjun95@gmail.com>
2025-03-30 13:03:50 +0530

    feat: add tests for token request functionality in ERC20Faucet contract

--- hardhat/test/Faucet.ts
+++ hardhat/test/Faucet.ts
@@ -58,4 +58,63 @@ describe("ERC20Faucet Contract", () => {
       expect(faucetBalance).to.be.equal(mintBalance);
     });
   });
+
+  describe("Request tokens", () => {
+    let tx: ContractTransactionResponse;
+    const mintBalance = parseUnits(100_000);
+
+    beforeEach(async () => {
+      // Mint some tokens to Faucet
+      await usdtToken.mint(await faucet.getAddress(), mintBalance);
+
+      // Request some tokens
+      tx = await faucet.connect(addr1 as any).requestTokens();
+      await tx.wait();
+    });
+
+    it("Should decrease faucet balance", async () => {
+      // Faucet Balance should decrease by withdrawal amount
+      expect(await usdtToken.balanceOf(await faucet.getAddress())).to.be.equal(
+        mintBalance - FAUCET.withdrawlAmount
+      );
+    });
+
+    it("Should increase user balance", async () => {
+      // User Balance should increase by withdrawal amount
+      expect(await usdtToken.balanceOf(addr1.address)).to.be.equal(
+        FAUCET.withdrawlAmount
+      );
+    });
+
+    it("Should have correct nextAccessTime", async () => {
+      const prevAccessTime = BigInt((await tx.getBlock())?.timestamp || 0);
+      const nextAccessTime = await faucet.getNextAccessTime(addr1.address);
+
+      // Verify nextAccessTime
+      expect(nextAccessTime - prevAccessTime).to.be.equal(FAUCET.lockTime);
+    });
+
+    it("Should emit event", async () => {
+      await expect(tx)
+        .to.be.emit(faucet, "Faucet__ReceivedFunds")
+        .withArgs(
+          await faucet.getAddress(),
+          addr1.address,
+          FAUCET.withdrawlAmount,
+          (
+            await tx.getBlock()
+          )?.timestamp
+        );
+    });
+
+    it("Should wait till next access time", async () => {
+      // Requesting second time
+      await expect(
+        faucet.connect(addr1 as any).requestTokens()
+      ).to.be.revertedWithCustomError(
+        faucet,
+        "Faucet__InsufficientTimeElapsed"
+      );
+    });
+  });
 });
