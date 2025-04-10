708f190ed211b9c04d1dddea6bc504f41a92aac8
Arjun Porwal <porwalarjun95@gmail.com>
2025-04-10 14:54:30 +0530

    feat: implement addLiquidity function with ratio checks and event emission

--- hardhat/contracts/pool/ERC20/ERC20SwapPool.sol
+++ hardhat/contracts/pool/ERC20/ERC20SwapPool.sol
@@ -3,15 +3,18 @@
 pragma solidity ^0.8.28;
 
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/math/Math.sol";
 
 import {ERC20Token} from "../../token/Token.sol";
 import {LpToken} from "../../token/LpToken.sol";
 import "./IERC20SwapPool.sol";
 
 contract ERC20SwapPool is IERC20SwapPool, ReentrancyGuard {
-    ERC20Token private token1;
-    ERC20Token private token2;
-    LpToken private lpToken;
+    using Math for uint256;
+
+    ERC20Token private immutable token1;
+    ERC20Token private immutable token2;
+    LpToken private immutable lpToken;
     uint private fee; // if 100, it means 1%
 
     constructor(
@@ -38,12 +41,85 @@ contract ERC20SwapPool is IERC20SwapPool, ReentrancyGuard {
         lpToken = new LpToken(_lpTokenName, _lpTokenSymbol, _lpTokenLogo);
     }
 
+    function addLiquidity(
+        uint256 amountToken1,
+        uint256 amountToken2
+    ) external nonReentrant returns (uint256) {
+        (uint256 reserve_1, uint256 reserve_2) = getReserves();
+
+        if (reserve_1 > 0 || reserve_2 > 0) {
+            // This check must exist inorder to prevent price impact on adding liquidity
+            /*****
+                 reserve_1      amountToken1
+                ------------ = --------------
+                 reserver_2     amountToken2
+            /******/
+
+            (bool successLeft, uint256 leftSide) = reserve_1.tryMul(
+                amountToken2
+            );
+            (bool successRight, uint256 rightSide) = amountToken1.tryMul(
+                reserve_2
+            );
+
+            if (!(successLeft && successRight)) {
+                revert ERC20SwapPool__OverflowInRatioCheck(
+                    "Overflow in ratio check"
+                );
+            }
+
+            if (leftSide != rightSide) {
+                revert ERC20SwapPool__InvalidTokenRatio("Invalid token ratio");
+            }
+        }
+
+        // Transfer tokens to pool
+        token1.transferFrom(msg.sender, address(this), amountToken1);
+        token2.transferFrom(msg.sender, address(this), amountToken2);
+
+        uint256 mintingLpTokens = 0;
+        uint256 totalSupply = lpToken.totalSupply();
+
+        // Mint LP tokens
+        if (totalSupply == 0) {
+            mintingLpTokens = Math.sqrt(amountToken1 * amountToken2);
+        } else {
+            mintingLpTokens = Math.min(
+                (amountToken1 * totalSupply) / reserve_1,
+                (amountToken2 * totalSupply) / reserve_2
+            );
+        }
+
+        if (mintingLpTokens == 0) {
+            revert ERC20SwapPool__MintingLpTokensTooLess(
+                "Can not mint 0 LP tokens"
+            );
+        }
+
+        lpToken.mint(msg.sender, mintingLpTokens);
+
+        emit LiquidityAdded(
+            address(this),
+            address(token1),
+            address(token2),
+            amountToken1,
+            amountToken2,
+            mintingLpTokens,
+            token1.balanceOf(address(this)),
+            token2.balanceOf(address(this)),
+            block.timestamp,
+            msg.sender
+        );
+
+        return mintingLpTokens;
+    }
+
     /******************** Getters ********************/
     function getTokens() public view returns (address, address) {
         return (address(token1), address(token2));
     }
 
-    function getReserves() external view returns (uint256, uint256) {
+    function getReserves() public view returns (uint256, uint256) {
         return (
             token1.balanceOf(address(this)),
             token2.balanceOf(address(this))
--- hardhat/contracts/pool/ERC20/IERC20SwapPool.sol
+++ hardhat/contracts/pool/ERC20/IERC20SwapPool.sol
@@ -7,6 +7,30 @@ interface IERC20SwapPool {
 
     error ERC20SwapPool__FeeMoreThanThreshold(string description);
 
+    error ERC20SwapPool__OverflowInRatioCheck(string description);
+
+    error ERC20SwapPool__InvalidTokenRatio(string description);
+
+    error ERC20SwapPool__MintingLpTokensTooLess(string description);
+
+    event LiquidityAdded(
+        address indexed pool,
+        address indexed token1,
+        address indexed token2,
+        uint256 tokenAmount1,
+        uint256 tokenAmount2,
+        uint256 mintedLpTokens,
+        uint256 reserveToken1,
+        uint256 reserveToken2,
+        uint256 timestamp,
+        address sender
+    );
+
+    function addLiquidity(
+        uint256 _amountTokenA,
+        uint256 _amountTokenB
+    ) external returns (uint256);
+
     function getTokens() external view returns (address, address);
 
     function getReserves() external view returns (uint256, uint256);
