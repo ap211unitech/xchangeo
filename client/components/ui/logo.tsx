2bf8454cae49098971c7697ad6a847707fa8ab69
Arjun Porwal <porwalarjun95@gmail.com>
2025-06-04 17:57:35 +0530

    feat: update logo gradient colors and enhance text styling

--- client/components/ui/logo.tsx
+++ client/components/ui/logo.tsx
@@ -13,7 +13,7 @@ export const XchangeoLogo = ({ isFull = false }: Props) => (
     <svg width="0" height="0">
       <defs>
         <linearGradient id="gradient" x1="0%" y1="0%" x2="100%" y2="0%">
-          <stop stopColor="#ff6ec4" offset="0%" />
+          <stop stopColor="#fb2178" offset="0%" />
           <stop stopColor="#7873f5" offset="100%" />
         </linearGradient>
       </defs>
@@ -23,7 +23,9 @@ export const XchangeoLogo = ({ isFull = false }: Props) => (
         <X className="absolute inset-0 h-full w-full -rotate-6 stroke-[1]" style={{ stroke: "url(#gradient)" }} />
         <X className="absolute inset-0 h-full w-full rotate-6 stroke-[1]" style={{ stroke: "url(#gradient)" }} />
       </div>
-      {isFull && <p className="relative -top-0.5 -left-4 text-4xl text-[#7873f5]">changeo</p>}
+      {isFull && (
+        <p className="relative -top-0.5 -left-4 bg-gradient-to-l from-[#fb2178] to-[#7873f5] bg-clip-text text-4xl text-transparent">changeo</p>
+      )}
     </div>
   </>
 );
