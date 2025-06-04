c80f2ffab884362ad47b3655689d206d00013b1d
Arjun Porwal <porwalarjun95@gmail.com>
2025-06-04 17:04:08 +0530

    feat: make logo clickable and style changes

--- client/components/ui/header/index.tsx
+++ client/components/ui/header/index.tsx
@@ -1,12 +1,16 @@
+import Link from "next/link";
+
 import { Button, XchangeoLogo } from "@/components/ui";
 
 import { Navigation } from "./navigation";
 
 export const Header = () => {
   return (
-    <header className="flex items-center justify-between px-4 py-2">
+    <header className="bg-sidebar flex items-center justify-between px-4 py-2">
       <div className="flex items-center">
-        <XchangeoLogo isFull />
+        <Link href="/">
+          <XchangeoLogo isFull />
+        </Link>
         <Navigation />
       </div>
       <div>
--- client/components/ui/header/navigation.tsx
+++ client/components/ui/header/navigation.tsx
@@ -1,5 +1,6 @@
 import { CircleCheckIcon, CircleHelpIcon, CircleIcon } from "lucide-react";
 import Link from "next/link";
+import { v4 as uuidv4 } from "uuid";
 
 import {
   NavigationMenu,
@@ -10,39 +11,47 @@ import {
   NavigationMenuTrigger,
 } from "@/components/ui/navigation-menu";
 
+const exploreLinks = [
+  {
+    title: "Tokens",
+    description: "Explore available tokens.",
+    href: "/explore/tokens",
+  },
+  {
+    title: "Faucets",
+    description: "Request test tokens for trading.",
+    href: "/explore/faucets",
+  },
+  {
+    title: "Transactions",
+    description: "View faucet transaction history.",
+    href: "/explore/transactions",
+  },
+];
+
 export const Navigation = () => {
   return (
     <NavigationMenu delayDuration={0} viewport={false}>
       <NavigationMenuList>
         <NavigationMenuItem>
-          <NavigationMenuTrigger>Explore</NavigationMenuTrigger>
+          <NavigationMenuTrigger className="bg-transparent text-base">Explore</NavigationMenuTrigger>
           <NavigationMenuContent>
             <ul className="grid w-[250px] gap-4">
               <li>
-                <NavigationMenuLink asChild>
-                  <Link href="#">
-                    <div className="font-medium">Tokens</div>
-                    <div className="text-muted-foreground">Explore available tokens.</div>
-                  </Link>
-                </NavigationMenuLink>
-                <NavigationMenuLink asChild>
-                  <Link href="#">
-                    <div className="font-medium">Faucets</div>
-                    <div className="text-muted-foreground">Request test tokens for trading.</div>
-                  </Link>
-                </NavigationMenuLink>
-                <NavigationMenuLink asChild>
-                  <Link href="#">
-                    <div className="font-medium">Transactions</div>
-                    <div className="text-muted-foreground">View faucet transaction history.</div>
-                  </Link>
-                </NavigationMenuLink>
+                {exploreLinks.map(({ title, description, href }) => (
+                  <NavigationMenuLink key={uuidv4()} asChild>
+                    <Link href={href}>
+                      <div className="font-medium">{title}</div>
+                      <div className="text-muted-foreground">{description}</div>
+                    </Link>
+                  </NavigationMenuLink>
+                ))}
               </li>
             </ul>
           </NavigationMenuContent>
         </NavigationMenuItem>
         <NavigationMenuItem>
-          <NavigationMenuTrigger>With Icon</NavigationMenuTrigger>
+          <NavigationMenuTrigger className="bg-transparent text-base">With Icon</NavigationMenuTrigger>
           <NavigationMenuContent>
             <ul className="grid w-[200px] gap-4">
               <li>
