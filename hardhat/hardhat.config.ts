7e3b1972bf0ec9e83f837e4900a14efa4b02f944
Arjun Porwal <porwalarjun95@gmail.com>
2025-09-27 12:02:20 +0530

    🚀 feat: deployed contracts to sepolia testnet

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,5 +1,9 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, vars } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+
+const ALCHEMY_API_KEY = vars.get("ALCHEMY_API_KEY");
+const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY");
+const SEPOLIA_ACCOUNT_PRIVATE_KEY = vars.get("SEPOLIA_ACCOUNT_PRIVATE_KEY");
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -10,6 +14,15 @@ const config: HardhatUserConfig = {
         runs: 1000,
       },
       viaIR: true,
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [SEPOLIA_ACCOUNT_PRIVATE_KEY],
     },
   },
 };
